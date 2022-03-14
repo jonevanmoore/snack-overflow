bb3b410ed016a39cf1f904decc5520115d1f9a84
MasonKogami <maskogami@gmail.com>
2022-03-14 14:46:00 -0700

    signup
    Co-authored-by: Sam Kramer <SamuelRKramer@gmail.com>
    Co-authored-by: Jon Moore <jonevanmoore@yahoo.com>
    Co-authored-by: Mason Kogami <maskogami@gmail.com>
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

--- express_project/routes/users.js
+++ express_project/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/*GET /signup form */
+router.get('/signup', (req, res) => {
+  const {username, hashed_password, email, bio} = req.body;
+  res.render('', {})
 });
 
 module.exports = router;
