ee226b24449677cc3b7662c1322ebe69da542e88
MasonKogami <maskogami@gmail.com>
2022-03-15 11:18:00 -0700

    questionsRouter post has been added, and pug file will be updated to display errors array
    Co-authored-by: Sam Kramer <SamuelRKramer@gmail.com>
    Co-authored-by: Jon Moore <jonevanmoore@yahoo.com>
    Co-authored-by: Mason Kogami <maskogami@gmail.com>
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

--- express_project/routes/questions.js
+++ express_project/routes/questions.js
@@ -2,7 +2,6 @@ const express = require('express');
 const csrf = require('csurf');
 const { check, validationResult } = require('express-validator');
 const { Question } = require('../db/models');
-const { restoreUser } = require('../auth');
 const router = express.Router();
 
 const csrfProtection = csrf({cookie: true});
@@ -10,6 +9,35 @@ const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).ca
 
 router.get('/', csrfProtection, asyncHandler( async(req, res) => {
     res.render('question-create', {token: req.csrfToken()});
+}));
+
+const questionValidator = [
+    check("title")
+        .exists({checkFalsy: true})
+        .withMessage("Please input a title.")
+        .isLength({max: 255})
+        .withMessage("Title must be 255 characters or fewer."),
+    check("body")
+        .exists({checkFalsy: true})
+        .withMessage("Please input a body.")
+];
+
+router.post('/', questionValidator, csrfProtection, asyncHandler( async(req, res, next) => {
+    const {title, body, image_link1} = req.body;
+    const question = Question.build({
+        title,
+        body,
+        image_link1
+    });
+
+    const validateErrors = validationResult(req);
+
+    if (validateErrors.isEmpty()) {
+        const newQ = await question.save();
+        res.redirect(`/questions/${newQ.id}`)
+    } else {
+        const errors = validateErrors.array()
+    }
 }));
 
 module.exports = router;
