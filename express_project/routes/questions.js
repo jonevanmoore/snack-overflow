989da4690e711364bf3b31ab47d7e3b6c6b93713
MasonKogami <maskogami@gmail.com>
2022-03-15 13:38:18 -0700

    post question handler working
    Co-authored-by: Sam Kramer <SamuelRKramer@gmail.com>
    Co-authored-by: Jon Moore <jonevanmoore@yahoo.com>
    Co-authored-by: Mason Kogami <maskogami@gmail.com>
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

--- express_project/routes/questions.js
+++ express_project/routes/questions.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const csrf = require('csurf');
 const { check, validationResult } = require('express-validator');
-const { Question } = require('../db/models');
+const { Question, User } = require('../db/models');
 const router = express.Router();
 
 const csrfProtection = csrf({cookie: true});
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
-router.get('/', csrfProtection, asyncHandler( async(req, res) => {
-    res.render('question-create', {token: req.csrfToken()});
+router.get('/new', csrfProtection, asyncHandler( async(req, res) => {
+    if (req.session.auth) {
+        res.render('question-create', {token: req.csrfToken()});
+    } else {
+        res.render('user-login', {title: 'Login to ask a question', token: req.csrfToken()})
+    }
 }));
 
 const questionValidator = [
@@ -22,12 +26,15 @@ const questionValidator = [
         .withMessage("Please input a body.")
 ];
 
-router.post('/', questionValidator, csrfProtection, asyncHandler( async(req, res, next) => {
-    const {title, body, image_link1} = req.body;
+router.post('/new', questionValidator, csrfProtection, asyncHandler( async(req, res, next) => {
+    const {title, body, image_link1, image_link2, image_link3} = req.body;
     const question = Question.build({
+        user_id: res.locals.user.id,
         title,
         body,
-        image_link1
+        image_link1,
+        image_link2,
+        image_link3
     });
 
     const validateErrors = validationResult(req);
@@ -36,7 +43,12 @@ router.post('/', questionValidator, csrfProtection, asyncHandler( async(req, res
         const newQ = await question.save();
         res.redirect(`/questions/${newQ.id}`)
     } else {
-        const errors = validateErrors.array()
+        const errors = validateErrors.array().map(error => error.msg);
+        res.render('question-create', {
+            question,
+            errors,
+            token: req.csrfToken()
+        });
     }
 }));
 
