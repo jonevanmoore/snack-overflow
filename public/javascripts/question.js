7ada030988a14e1cbbfe7aa29c35bac9666277ae
Jon Moore <jonevanmoore@yahoo.com>
2022-03-17 15:14:54 -0500

    minor bug on specific question page
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

---
+++ public/javascripts/question.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    const editButtons = document.querySelectorAll(".answer-edit")
+
+
+    editButtons.forEach(button => {
+        const id = button.id.split('-')[2]
+        button.addEventListener('click', e => {
+
+        })
+    })
+
+})
