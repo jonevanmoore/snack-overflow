887aa230bd0a83d89d5c00e172179d43284e388e
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-03-18 10:37:45 -0500

    create unique constraint migrations
    Co-authored-by: Sam Kramer <SamuelRKramer@gmail.com>
    Co-authored-by: Jon Moore <jonevanmoore@yahoo.com>
    Co-authored-by: Mason Kogami <maskogami@gmail.com>
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

---
+++ db/migrations/20220318152528-add-unique-to-user-column-email.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+        return queryInterface.changeColumn('Users','email', { 
+          type: Sequelize.DataTypes.STRING(20),
+          unique: true,
+          allowNull: false
+        });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+        return queryInterface.changeColumn('Users','email', { 
+          type: Sequelize.DataTypes.STRING(20),
+          unique: false,
+          allowNull: false
+        });
+  }
+};
