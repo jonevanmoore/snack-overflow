bceade949414b2bc2ef53f3e5f9481872537c0cf
Jon Moore <jonevanmoore@yahoo.com>
2022-03-18 18:11:27 -0500

    sam link

--- public/javascripts/sign-up-modal.js
+++ public/javascripts/sign-up-modal.js
@@ -1,25 +1,33 @@
 window.addEventListener("DOMContentLoaded", () => {
-
+    // MODAL STUFF
     const btn = document.getElementById("modalBtn");
-
     const modal = document.getElementById("myModal");
-
     const span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal
     btn.onclick = function () {
         modal.style.display = "block";
     }
 
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
+    // MODAL STUFF END
+
+    const usernameInput = document.getElementByClassName('username-input')
+    const signUpBtn = document.getElementByClassName('sign-up-button')
+    usernameInput.style.backgroundColor = 'red'
+
+    usernameInput.addEventListener('input', e => {
+        if (e.target.value === 'jonevanmoore') {
+            signUpBtn.onclick = ev => {
+                ev.preventDefault()
+            }
+        }
+    })
 })
